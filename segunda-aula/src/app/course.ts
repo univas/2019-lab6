7beae68fc9781902456b96cbfb29622c78481f4e
Rodrigo Luis de Faria <rodrigo.faria@inatel.br>
2019-10-02 21:12:09 -0300

    Fix problem to save data

--- segunda-aula/src/app/course.ts
+++ segunda-aula/src/app/course.ts
@@ -1,32 +1,14 @@
 export class Course {
 
-    private _id: number;
-    private _name: string;
-    private _workload: number;
+    id: number;
+    name: string;
+    workload: number;
 
     constructor(id?: number, name?: string, workload?: number) {
-        this._id = id;
-        this._name = name;
-        this._workload = workload;
+        this.id = id;
+        this.name = name;
+        this.workload = workload;
     }
 
-    public get id(): number {
-        return this._id;
-    }
-    public set id(value: number) {
-        this._id = value;
-    }
-    public get name(): string {
-        return this._name;
-    }
-    public set name(value: string) {
-        this._name = value;
-    }
-    public get workload(): number {
-        return this._workload;
-    }
-    public set workload(value: number) {
-        this._workload = value;
-    }
 
 }
